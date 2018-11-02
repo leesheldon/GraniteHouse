de098817776364cf885b62a3b79a610a26fcf20b
Han Co <lhanco@gmail.com>
2018-11-02 12:50:55 +0700

    Add project files.

---
+++ GraniteHouse/Models/ProductTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraniteHouse.Models
+{
+    public class ProductTypes
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
